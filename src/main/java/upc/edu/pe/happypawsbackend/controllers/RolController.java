4b7fd75f32510638ebbff197fb1fa7ca6b523014
SebasR1C0 <102307907+SebasR1C0@users.noreply.github.com>
2024-09-26 19:28:41 -0500

    Initial commit

---
+++ src/main/java/upc/edu/pe/happypawsbackend/controllers/RolController.java
@@ -0,0 +1,65 @@
+package upc.edu.pe.happypawsbackend.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import upc.edu.pe.happypawsbackend.dtos.RolByUserDTO;
+import upc.edu.pe.happypawsbackend.dtos.RoleDTO;
+import upc.edu.pe.happypawsbackend.entities.Role;
+import upc.edu.pe.happypawsbackend.serviceinterfaces.IRoleService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/roles")
+
+public class RolController {
+    @Autowired
+    private IRoleService roleService;
+
+    @GetMapping
+    @PreAuthorize("hasAuthority('ADMINISTRADOR')")
+    public List<RoleDTO> listar() {
+        return roleService.list().stream().map(x -> {
+            ModelMapper m = new ModelMapper();
+            return m.map(x, RoleDTO.class);
+        }).collect(Collectors.toList());
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('ADMINISTRADOR')")
+    public void insertar(@RequestBody RoleDTO roledto) {
+        ModelMapper m = new ModelMapper();
+        Role role = m.map(roledto, Role.class);
+        roleService.insert(role);
+    }
+    @PutMapping
+    @PreAuthorize("hasAuthority('ADMINISTRADOR')")
+    public void modificar(@RequestBody RoleDTO roledto) {
+        ModelMapper m = new ModelMapper();
+        Role role = m.map(roledto, Role.class);
+        roleService.update(role);
+    }
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMINISTRADOR')")
+    public void eliminar(@PathVariable("id") Long id){
+        roleService.delete(id);
+    }
+
+    @GetMapping("/rolxuser")
+    @PreAuthorize("hasAuthority('ADMINISTRADOR')")
+    public List<RolByUserDTO> rolporuser(){
+        List<String[]> lista = roleService.countrol();
+        List<RolByUserDTO> ListDTO = new ArrayList<>();
+        for (String[] columna:lista) {
+            RolByUserDTO dto = new RolByUserDTO();
+            dto.setNombreRol(columna[0]);
+            dto.setCount(Integer.parseInt(columna[1]));
+            ListDTO.add(dto);
+        }
+        return ListDTO;
+    }
+}
---
+++ src/main/java/upc/edu/pe/happypawsbackend/serviceinterfaces/IComprobanteService.java
@@ -0,0 +1,12 @@
+package upc.edu.pe.happypawsbackend.serviceinterfaces;
+
+import upc.edu.pe.happypawsbackend.entities.Comprobante;
+
+import java.util.List;
+
+public interface IComprobanteService {
+    public List<Comprobante> list();
+    public void insert(Comprobante comprobante);
+    public void update(Comprobante comprobante);
+    public void delete(int comprobanteId);
+}
